335923c820092517ce5acf032dcb7eea94d24294
WhizSid <whizsid@aol.com>
2019-09-03 02:16:35 +0530

    Updated the link

--- resources/ts/components/GuestPanel/Layout/Header.tsx
+++ resources/ts/components/GuestPanel/Layout/Header.tsx
@@ -6,8 +6,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/styles/withStyles";
 import Toolbar from "@material-ui/core/Toolbar";
-// import red from "@material-ui/core/colors/red";
-
+import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 
 import LoginForm from "./Header/LoginForm";
@@ -15,7 +14,6 @@ import UserArea from "./Header/UserArea";
 import { APP_NAME, APP_URL } from "../../../constants/config";
 import { UserInformations } from "../../../store/mainTypes";
 import { AppState } from "../../../rootReducer";
-import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 export const styler = withStyles(({ spacing }) => ({
@@ -86,7 +84,7 @@ class Header extends React.Component<Props> {
                                 <Typography variant="h6">{APP_NAME}</Typography>
                             </a>
                             <div className={classes.grow} />
-                            <Button href="https://github.com/whizsid/AbandonedSeed" className={classes.iconButton}>
+                            <Button href="https://github.com/PayingForward/web" className={classes.iconButton}>
                                 <img width="24px" src={APP_URL+"images/github.svg"} className={classes.iconButton} />
                                 Contribute
                             </Button>
